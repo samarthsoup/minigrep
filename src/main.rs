c957dfdd8e4116016e9d7083067c5f225f41c1d8
samarth <samarthkulkarni@icloud.com>
2024-01-30 14:46:51 +0530

    separating function to store env args from main

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = parse_config(&args);
 
     println!("searching for {}", query);
     println!("in file {}", file_path);
@@ -14,4 +13,19 @@ fn main() {
         .expect("should have been able to read the file");
 
     println!("with text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = &args[1].clone();
+    let file_path = &args[2].clone();
+
+    Config {
+        query, 
+        file_path
+    }
 }
